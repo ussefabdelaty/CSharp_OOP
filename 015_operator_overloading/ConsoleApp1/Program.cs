8539f041c989b8c3a9ce6e43c803816b0ca1a609
unknown <ussifyounis@gmail.com>
2023-10-14 16:55:02 +0300

    this folder contains examples on types of operator overloading

---
+++ 015_operator_overloading/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money m1 = new Money(10);
+            Money m2 = new Money(20);
+
+            Money m3 = m1 + m2;
+           Console.WriteLine($"money m3 :${ m3.Amount}");
+            Console.WriteLine(++m3.Amount);
+        }
+    }
+
+    class Money
+    {
+        private decimal amount;
+
+        public Money(decimal value)
+        {
+            this.amount = value;
+        }
+        public decimal Amount
+        {
+            get 
+            {
+              return amount;
+            }
+            set {
+                amount = value; 
+            }
+        }
+
+
+
+
+        public static Money operator+(Money m1 , Money m2) {
+              var value = m1.Amount + m2.Amount;
+              return new  Money(value);                    
+          }
+
+         public static bool operator >(Money m1 , Money m2)
+          {
+              return m1.Amount > m2.Amount;   
+          }
+         public static bool operator <(Money m1, Money m2)
+          {
+              return m1.Amount > m2.Amount;
+          }
+
+        public static bool operator ==(Money m1, Money m2)
+        {
+            return m1.Amount > m2.Amount;
+        }
+        public static bool operator !=(Money m1, Money m2)
+        {
+            return m1.Amount > m2.Amount;
+        }
+        public static Money operator ++(Money money)
+        {
+           var value = money.Amount;
+            return new Money(++value);
+        }
+        public static Money operator --(Money money)
+        {
+            var value = money.Amount;
+            return new Money(--value);  
+        }
+
+    }
+}
