3c1ccf72f887b29562e439a4c9c95afce19af4b7
unknown <ussifyounis@gmail.com>
2023-10-14 17:00:31 +0300

    examples on types of errors

---
+++ 018_Trouble_Shoot/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//            var amount = 1000;
+//            var members = 2;
+
+//            members = Reduce(members, 2)
+//;
+//            var share =Distributed(amount, members); 
+//            Console.WriteLine(share);    
+        }
+        //static int Reduce (int members , int value)
+        //{
+        //    return members -= value;
+        //}
+
+        //static int Distributed(int members , int amount)
+        //{
+        //    try
+        //    {
+        //        return amount / members;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine($"unexpectec error: {ex.Message} ");
+        //    }
+        //    return 0;   
+        //}
+    }
+}
