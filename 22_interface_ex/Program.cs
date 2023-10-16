8183e43475eab5cd5332f46af25fc74b4f1af0f3
unknown <ussifyounis@gmail.com>
2023-10-16 14:54:23 +0300

    a simple example on interface

---
+++ 22_interface_ex/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _22_interface_ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cashier c = new Cashier(new Cash());
+            c.CheckOut(6568643);
+            Console.ReadKey();
+        }
+    }
+    class Cashier
+    {
+        private IPayment _payment;
+        public Cashier(IPayment payment)
+        {
+            _payment = payment ; 
+        }
+        public void CheckOut (decimal amount)
+        {
+            _payment.pay(amount);
+        }
+    }
+    interface IPayment
+    {
+        void pay(decimal amount);   
+    }
+    class Cash :IPayment
+    {
+        public void pay(decimal amount)
+        {
+            Console.WriteLine($"cash payment: {Math.Round(amount, 2):N0}");
+        }
+    }
+    class Debit:IPayment
+    {
+        public void pay(decimal amount)
+        {
+            Console.WriteLine($"debit payment: {Math.Round(amount, 2):N0}");
+        }
+    }
+    class Visa : IPayment   
+    {
+        public void pay(decimal amount)
+        {
+            Console.WriteLine($"Visa payment: {Math.Round(amount, 2):N0}");
+        }
+    }
+    class MasterCard : IPayment 
+    {
+        public void pay(decimal amount)
+        {
+            Console.WriteLine($"master card payment: {Math.Round(amount, 2):N0}");
+        }
+    }
+}
---
+++ 22_interface_ex/ReadMe.txt
@@ -0,0 +1,5 @@
+Interface Example 
+
+-a simple example on how to use interface specially "loose coupling".
+
+-the examply talking about how to apply difference payment.
